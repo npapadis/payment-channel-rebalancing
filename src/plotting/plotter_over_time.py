a089a4a3c7b27e985af841ec40874abea4307fb4
Nikos Papadis <79573408+npapadis@users.noreply.github.com>
2022-08-17 16:39:49 +0300

    Added script for drawing all over-time graphs at once.

---
+++ src/plotting/plotter_over_time.py
@@ -0,0 +1,11 @@
+from src.plotting.reb_plotter_losses_over_time import plot_fee_losses_over_time
+from src.plotting.reb_plotter_over_time import plot_total_fortune_over_time
+from src.plotting.reb_plotter_swaps_over_time import plot_number_of_swaps_over_time
+
+if __name__ == '__main__':
+
+    filename = 'results_104_uniform'
+
+    plot_total_fortune_over_time(filename)
+    plot_fee_losses_over_time(filename)
+    plot_number_of_swaps_over_time(filename)
